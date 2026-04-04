773c5b0384e7cac482141108d3665e99dbfd7323
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-04 13:43:11 +0530

    Add solution for Concatenation of Array

---
+++ solutions/easy/Concatenation of Array.java
@@ -0,0 +1,17 @@
+// Title: Concatenation of Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/concatenation-of-array/
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int [] ans =new int[n*2];
+
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+        }
+    }
+            ans[n+i]=nums[i];
+        return ans;
+}
