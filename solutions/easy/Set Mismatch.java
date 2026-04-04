12a5702f74769ea8b7b3546b7a7be3fffa6f35d0
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-04 13:57:55 +0530

    Add solution for Set Mismatch

---
+++ solutions/easy/Set Mismatch.java
@@ -0,0 +1,21 @@
+// Title: Set Mismatch
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/set-mismatch/
+
+            if (freq[i] == 0) {
+        }
+        // Find duplicate and missing
+
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) {
+            }
+                ans[1] = i; // missing
+                ans[0] = i; // duplicate
+            }
+        }
+            freq[num]++;
+        for (int num : nums) {
+        // Count frequency
+
+        int[] ans = new int[2];
