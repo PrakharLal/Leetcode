3a699206a0647e94ce724ca5196f6f20efea97e4
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-05 10:37:35 +0530

    Add solution for Top K Frequent Elements

---
+++ solutions/medium/Top K Frequent Elements.java
@@ -0,0 +1,14 @@
+// Title: Top K Frequent Elements
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/top-k-frequent-elements/
+
+        }
+}
+int[]res=new int[k];
+
+while(!pq.isEmpty()){
+    res[i++]=pq.poll().second;
+    
+}
+int i=0;
