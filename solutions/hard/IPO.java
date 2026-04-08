198856fc2f3af576d944001df345a16a96ed3769
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-08 12:21:43 +0530

    Add solution for IPO

---
+++ solutions/hard/IPO.java
@@ -0,0 +1,20 @@
+// Title: IPO
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/ipo/
+
+                idx++;
+            }
+        }
+
+        w+=pq.poll();
+        if(pq.isEmpty()) break;
+
+        }
+        //edge case if pq is empty
+
+
+        return w;
+    
+    }
+}
