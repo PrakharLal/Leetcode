eef87b5157eb47a5f02b897d4d4eb30890dc04ba
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-11 21:04:27 +0530

    Add solution for Contains Duplicate

---
+++ solutions/easy/Contains Duplicate.java
@@ -0,0 +1,18 @@
+// Title: Contains Duplicate
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/contains-duplicate/
+
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+       
+        return false;
+    }
+        for(int num:nums){
+
+        HashMap<Integer,Integer> map= new HashMap<>();
+            if(map.get(num)>=2) return true;
+}
+    public boolean containsDuplicate(int[] nums) {
+class Solution {
