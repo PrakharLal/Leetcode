4fea697e6b80d96ed341d68c4351a1e2a68446af
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-13 13:12:46 +0530

    Add solution for Binary Tree Level Order Traversal

---
+++ solutions/medium/Binary Tree Level Order Traversal.java
@@ -0,0 +1,26 @@
+// Title: Binary Tree Level Order Traversal
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+        if(root==null) return wrapList;
+
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+
+        List<List<Integer>> wrapList= new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+class Solution {
+ */
+ * }
+ *     }
+ *         this.right = right;
+ *         this.left = left;
+ *         this.val = val;
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode() {}
+ *     TreeNode right;
+ *     TreeNode left;
