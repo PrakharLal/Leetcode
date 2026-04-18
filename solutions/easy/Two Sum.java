e8c7c82f812faa64a6ccc9d208fe9629269ea994
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-18 19:42:28 +0530

    Add solution for Two Sum

--- solutions/easy/Two Sum.java
+++ solutions/easy/Two Sum.java
@@ -3,6 +3,10 @@
             // Language: Java
             // Link: https://leetcode.com/problems/two-sum/
 
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         
         for(int i = 0; i < nums.length; i++) {
@@ -17,4 +21,3 @@
         
         return new int[]{-1, -1}; // just in case
     }
-}
