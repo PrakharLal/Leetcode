fe06bd12a605bee5746a7daf59a6658976a14a7d
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-22 20:54:37 +0530

    Add solution for Add Two Numbers

--- solutions/medium/Add Two Numbers.java
+++ solutions/medium/Add Two Numbers.java
@@ -3,6 +3,18 @@
             // Language: Java
             // Link: https://leetcode.com/problems/add-two-numbers/
 
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        ListNode dummy = new ListNode(0); // result head
+        ListNode curr = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            
+            int sum = carry;
+
+            if (l1 != null) {
                 sum += l1.val;
                 l1 = l1.next;
             }
@@ -17,7 +29,3 @@
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
         }
-
-        return dummy.next;
-    }
-}
