950c5ef5b5557bd30611aa20b41d6757177565e3
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-04-24 16:14:56 +0530

    Add solution for Design Underground System

---
+++ solutions/medium/Design Underground System.java
@@ -0,0 +1,23 @@
+// Title: Design Underground System
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/design-underground-system/
+
+        Trip trip= tripMap.get(route);
+
+        String route= startStation + "-" + endStation;
+    public double getAverageTime(String startStation, String endStation) {
+    
+    }
+        tripMap.put(route,trip);
+
+        trip.count +=1;
+        trip.totalTime+=travelTime;
+        Trip trip=  tripMap.getOrDefault(route, new Trip());
+
+        int travelTime = t - checkIn.time;
+        String route= checkIn.station + "-" + stationName;
+
+        checkInMap.remove(id);
+        CheckIn checkIn = checkInMap.get(id);
+    public void checkOut(int id, String stationName, int t) {
