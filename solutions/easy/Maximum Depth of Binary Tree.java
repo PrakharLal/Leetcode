52d66b25bb414ba3d38df054749de4bf99610449
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-05-01 10:39:10 +0530

    Add solution for Maximum Depth of Binary Tree

---
+++ solutions/easy/Maximum Depth of Binary Tree.java
@@ -0,0 +1,9 @@
+// Title: Maximum Depth of Binary Tree
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
