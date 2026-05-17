bba2af44c1cceaf6bfb44776c7ed29823e3a0411
prakhar <140285165+PrakharLal@users.noreply.github.com>
2026-05-17 09:01:31 +0530

    Add solution for Check Adjacent Digit Differences

---
+++ solutions/easy/Check Adjacent Digit Differences.java
@@ -0,0 +1,5 @@
+// Title: Check Adjacent Digit Differences
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/check-adjacent-digit-differences/
+
